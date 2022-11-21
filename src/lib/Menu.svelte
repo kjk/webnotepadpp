0e6ae9889824777dd87fcb8cf8dccdaa5ddf6e90
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2022-11-21 08:28:15 +0000

    change menu display to use table

--- src/lib/Menu.svelte
+++ src/lib/Menu.svelte
@@ -27,14 +27,17 @@
 
 <div class="mr-2 hover:bg-gray-300 cursor-pointer" on:click={toggleOpened}>{fixMenuName(def[0])}  
   {#if isOpened}
-    <div class="menu absolute flex flex-col bg-white px-2 py-2 border-2 z-40 text-sm">
+    <div class="table absolute flex flex-col bg-white px-2 py-2 border-2 z-40 text-sm">
           {#each def[1] as mi}
         {#if mi === menuDiv}
-          <hr class="mt-2 mb-2" on:click={eatClick}>
+          <div class="table-row mt-2 mb-2">
+            <hr class="table-cell" on:click={eatClick}>
+            <hr class="table-cell" on:click={eatClick}>
+          </div>
         {:else}
-            <div class="flex flex-row justify-between	 hover:bg-gray-100" on:click="{menuClicked(mi)}">
-                <div>{mi[0]}</div>
-                <div class="ml-4 text-gray-500">{mi[2] || " "}</div>
+            <div class="table-row hover:bg-gray-100" on:click="{menuClicked(mi)}">
+                <div class="table-cell">{mi[0]}</div>
+                <div class="table-cell pl-4 pr-2 text-gray-500">{mi[2] || " "}</div>
             </div>
         {/if}
           {/each}
@@ -43,7 +46,4 @@
 </div>
 
 <style>
-  .menu {
-    grid-template-columns: auto auto auto;
-  }
 </style>
--- src/menu-app.js
+++ src/menu-app.js
@@ -8,6 +8,16 @@ export const cmdFileSaveAs = "cmdFileSaveAs";
 export const cmdFileSaveCopyAs = "cmdFileSaveCopyAs";
 export const cmdFileSaveAll = "cmdFileSaveAll";
 export const cmdFileRename = "cmdFileRename";
+export const cmdFileClose = "cmdFileClose";
+export const cmdFileCloseAll = "cmdFileCloseAll";
+export const cmdFileMoveToRecycleBin = "cmdFileMoveToRecycleBin";
+export const cmdFileLoadSession = "cmdFileLoadSession";
+export const cmdFileSaveSession = "cmdFileSaveSession";
+export const cmdFilePrint = "cmdFilePrint";
+export const cmdFilePrintNow = "cmdFilePrintNow";
+export const cmdFileRestoreRecentClosedFile = "cmdFileRestoreRecentClosedFile";
+export const cmdFileOpenAllRecentFiles = "cmdFileOpenAllRecentFiles";
+export const cmdFileEmptyRecentFileList = "cmdFileEmptyRecentFileList";
 
 export const cmdEditUndo = "cmdEditUndo";
 export const cmdEditRedo = "cmdEditRedo";
@@ -36,6 +46,21 @@ const menuFile = [
   ["Save a Copy As...", cmdFileSaveCopyAs],
   ["Save All", cmdFileSaveAll, "Ctrl+Shift+S"],
   ["Rename...", cmdFileRename],
+  ["Close", cmdFileClose, "Ctrl+W"],
+  ["Close All", cmdFileCloseAll, "Ctrl+Shift+W"],
+  ["Close Multiple Documents", []],
+  ["Move to Recycle Bin"],
+  menuDiv,
+  ["Load Session...", cmdFileLoadSession],
+  ["Save Session...", cmdFileSaveSession],
+  menuDiv,
+  ["Print...", cmdFilePrint, "Ctrl+P"],
+  ["Print Now", cmdFilePrintNow],
+  menuDiv,
+  // TODO: recent files
+  ["Restore Recent Closed File", cmdFileRestoreRecentClosedFile, "Ctrl+Shift+T"],
+  ["Open All Recent Files", cmdFileOpenAllRecentFiles],
+  ["Empty Recent Files List", cmdFileEmptyRecentFileList]
 ];
 
 const menuEditInsert = [
@@ -58,8 +83,16 @@ const menuEdit = [
   ["Insert", menuEditInsert],
 ];
 
+export const cmdSearchFind = "cmdSearchFind";
+export const cmdSearchFindInFiles = "cmdSearchFindInFiles";
+export const cmdSearchFindNext = "cmdSearchFindNext";
+export const cmdSearchFindPrevious = "cmdSearchFindPrevious";
+
 const menuSearch = [
-  
+  ["Find", cmdSearchFind, "Ctrl+F"],
+  ["Find in Files...", cmdSearchFindInFiles, "Ctrl+Shift+f"],
+  ["Find Next", cmdSearchFindNext, "F3"],
+  ["Find Previous", cmdSearchFindPrevious, "Shift+F3"],
 ];
 
 const menuView = [
