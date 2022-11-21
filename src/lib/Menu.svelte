a9f0a86fdd4471defc7e5de274503ec6b8488517
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2022-11-21 07:37:43 +0000

    menu tweaks

--- src/lib/Menu.svelte
+++ src/lib/Menu.svelte
@@ -12,18 +12,29 @@
   function toggleOpened() {
     isOpened = !isOpened;
   }
+
+  function eatClick(ev) {
+    console.log("eat:", ev);  
+    ev.preventDefault();
+    ev.stopPropagation();
+  }
+
+  function menuClicked(mi) {
+    
+    console.log(mi);
+  }
 </script>
 
 <div class="mr-2 hover:bg-gray-300 cursor-pointer" on:click={toggleOpened}>{fixMenuName(def[0])}  
   {#if isOpened}
-    <div class="menu absolute grid bg-white px-2 py-2 border-2 z-40 text-sm">
+    <div class="menu absolute flex flex-col bg-white px-2 py-2 border-2 z-40 text-sm">
           {#each def[1] as mi}
         {#if mi === menuDiv}
-          <hr class="mt-2 mb-2">
+          <hr class="mt-2 mb-2" on:click={eatClick}>
         {:else}
-            <div class="flex flex-row justify-between	 hover:bg-gray-100 cursor-pointer">
+            <div class="flex flex-row justify-between	 hover:bg-gray-100" on:click="{menuClicked(mi)}">
                 <div>{mi[0]}</div>
-                <div class="mr-2 text-gray-500">{mi[2] || ""}</div>
+                <div class="ml-4 text-gray-500">{mi[2] || " "}</div>
             </div>
         {/if}
           {/each}
